888a9fada726e3eae69dc6d10374060e7ee4c4fc
eodrkd12 <37127443+eodrkd12@users.noreply.github.com>
2023-10-24 17:21:22 +0900

    feat: get work

--- src/app/@student/page.tsx
+++ src/app/@student/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
-import { Flex, HStack, useDisclosure, Text } from "@chakra-ui/react";
+import { Flex, HStack, Text, useDisclosure } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import { useCallback, useEffect, useRef, useState } from "react";
-import StudentModal from "./modal/student.modal";
-import videoStudentData from './data/videoStudent-data';
+import { useMediaQuery } from 'react-responsive';
 import digitalStudentData from './data/digitalStudent-data';
-import { filter } from 'lodash';
+import videoStudentData from './data/videoStudent-data';
+import StudentModal from "./modal/student.modal";
 
 interface ImageButtonProps {
     src: string;
@@ -31,6 +31,11 @@ export default function StudentScreen() {
 
     // 선택한 학생 정보 상태
     const [selectedStudent, setSelectedStudent] = useState<any>(null);
+
+    const isMobile = useMediaQuery({
+        query: '(max-width: 500px)'
+    });
+
 
     const {
         isOpen: isStudentModalOpen,
@@ -136,12 +141,12 @@ export default function StudentScreen() {
 
 
     const ImageButton = ({ src, alt, name, englishName, onClick }: ImageButtonProps) => (
-        <Flex style={{ position: 'relative', width: '14vw', border: '2px solid white' }}>
+        <Flex style={{ position: 'relative', width: isMobile ? '32vw' : '14vw', border: '2px solid white' }}>
             <img
                 src={src}
                 alt={alt}
                 style={{
-                    width: '14vw',
+                    width: isMobile ? '32vw' : '14vw',
                     height: '30vh',
                     transition: 'transform 0.3s, filter 0.3s',
                     filter: 'brightness(30%)'
@@ -174,19 +179,19 @@ export default function StudentScreen() {
                         animate={{ opacity: 1, y: 0 }}
                         transition={{ duration: 2 }}
                     >
-                        <Flex justifyContent={'center'} alignItems={'center'} ml={'13vw'} mr={'1vw'}>
-                            <button onMouseEnter={() => handleArrowMouseEnter('left')} onMouseLeave={handleArrowMouseLeave}
+                        <Flex justifyContent={'center'} alignItems={'center'} ml={isMobile ? 0 : '13vw'} mr={isMobile ? 0 : '1vw'}>
+                            {!isMobile && <button onMouseEnter={() => handleArrowMouseEnter('left')} onMouseLeave={handleArrowMouseLeave}
                                 style={{ fontSize: '5vw', color: isScrolledToLeft ? 'transparent' : 'white', height: '60vh', zIndex: 10 }} >
                                 <ChevronLeftIcon />
-                            </button>
-                            <HStack ref={studentRef} spacing={2} overflowX="scroll" h={'80vh'} p={'2%'} maxW={'71vw'} sx={{
+                            </button>}
+                            <HStack ref={studentRef} spacing={2} overflowX="scroll" h={'80vh'} p={isMobile ? 0 : '2%'} maxW={isMobile ? '100vw' : '71vw'} sx={{
                                 '::-webkit-scrollbar': {
                                     display: 'none',
                                 },
                             }}>
                                 <Flex flexDir={"column"} >
                                     <Flex >
-                                        <div style={{ width: '14vw', height: '30vh', borderTop: '2px solid white', flexShrink: 0 }}>
+                                        <div style={{ width: isMobile ? '32vw' : '14vw', height: '30vh', borderTop: '2px solid white', flexShrink: 0 }}>
                                             <div style={{ marginLeft: 15, color: 'white', fontSize: 25 }}>
                                                 VIDEO
                                                 <br />
@@ -194,23 +199,23 @@ export default function StudentScreen() {
                                             </div>
                                         </div>
                                         {videoStudentData.slice(0, Math.ceil(videoStudentData.length / 2 - 1)).map((student, index) => (
-                                            <ImageButton src={student.profile} alt="SignLogo" name={student.name} englishName={student.englishName} onClick={() => handleStudent(student)} />
+                                            <ImageButton key={index} src={student.profile} alt="SignLogo" name={student.name} englishName={student.englishName} onClick={() => handleStudent(student)} />
                                         )
                                         )}
                                     </Flex>
                                     <Flex>
                                         {videoStudentData.slice(Math.ceil(videoStudentData.length / 2 - 1)).map((student, index) => (
-                                            <ImageButton src={student.profile} alt="SignLogo" name={student.name} englishName={student.englishName} onClick={() => handleStudent(student)} />
+                                            <ImageButton key={index} src={student.profile} alt="SignLogo" name={student.name} englishName={student.englishName} onClick={() => handleStudent(student)} />
                                         )
                                         )}
 
                                     </Flex>
                                 </Flex>
                             </HStack>
-                            <button onMouseEnter={() => handleArrowMouseEnter('right')} onMouseLeave={handleArrowMouseLeave}
+                            {!isMobile && <button onMouseEnter={() => handleArrowMouseEnter('right')} onMouseLeave={handleArrowMouseLeave}
                                 style={{ fontSize: '5vw', color: isScrolledToRight ? 'transparent' : 'white', height: '60vh' }}  >
                                 <ChevronRightIcon />
-                            </button>
+                            </button>}
                         </Flex>
 
                     </motion.div>
@@ -237,44 +242,40 @@ export default function StudentScreen() {
                         animate={{ opacity: 1, y: 0 }}
                         transition={{ duration: 2 }}
                     >
-                        <Flex justifyContent={'center'} alignItems={'center'} ml={'13vw'} mr={'1vw'}>
-                            <button onMouseEnter={() => handleArrowMouseEnter2('left')} onMouseLeave={handleArrowMouseLeave2}
+                        <Flex justifyContent={'center'} alignItems={'center'} ml={isMobile ? 0 : '13vw'} mr={isMobile ? 0 : '1vw'}>
+                            {!isMobile && <button onMouseEnter={() => handleArrowMouseEnter2('left')} onMouseLeave={handleArrowMouseLeave2}
                                 style={{ fontSize: '5vw', color: isScrolledToLeft2 ? 'transparent' : 'white', height: '60vh', zIndex: 10 }} >
                                 <ChevronLeftIcon />
-                            </button>
-                            <HStack ref={studentRef2} spacing={2} overflowX="scroll" h={'80vh'} p={'2%'} maxW={'71vw'} sx={{
+                            </button>}
+                            <HStack ref={studentRef2} spacing={2} overflowX="scroll" h={'80vh'} p={isMobile ? 0 : '2%'} maxW={isMobile ? '100vw' : '71vw'} sx={{
                                 '::-webkit-scrollbar': {
                                     display: 'none',
                                 },
                             }}>
                                 <Flex flexDir={"column"} >
                                     <Flex >
-                                        <div style={{ width: '14vw', height: '30vh', borderTop: '2px solid white', flexShrink: 0 }}>
+                                        <div style={{ width: isMobile ? '32vw' : '14vw', height: '30vh', borderTop: '2px solid white', flexShrink: 0 }}>
                                             <div style={{ marginLeft: 15, color: 'white', fontSize: 25 }}>
-                                                Digital
+                                                DIGITAL
                                                 <br />
                                                 TRACK
                                             </div>
                                         </div>
                                         {digitalStudentData.slice(0, Math.ceil(digitalStudentData.length / 2 - 1)).map((student, index) => (
-                                            <ImageButton src={student.profile} alt="SignLogo" name={student.name} englishName={student.englishName} onClick={() => handleStudent(student)} />
-
-                                        )
-                                        )}
+                                            <ImageButton key={index} src={student.profile} alt="SignLogo" name={student.name} englishName={student.englishName} onClick={() => handleStudent(student)} />
+                                        ))}
                                     </Flex>
                                     <Flex>
                                         {digitalStudentData.slice(Math.ceil(digitalStudentData.length / 2 - 1)).map((student, index) => (
-                                            <ImageButton src={student.profile} alt="SignLogo" name={student.name} englishName={student.englishName} onClick={() => handleStudent(student)} />
-
-                                        )
-                                        )}
+                                            <ImageButton key={index} src={student.profile} alt="SignLogo" name={student.name} englishName={student.englishName} onClick={() => handleStudent(student)} />
+                                        ))}
                                     </Flex>
                                 </Flex>
                             </HStack>
-                            <button onMouseEnter={() => handleArrowMouseEnter2('right')} onMouseLeave={handleArrowMouseLeave2}
+                            {!isMobile && <button onMouseEnter={() => handleArrowMouseEnter2('right')} onMouseLeave={handleArrowMouseLeave2}
                                 style={{ fontSize: '5vw', color: isScrolledToRight2 ? 'transparent' : 'white', height: '60vh' }}  >
                                 <ChevronRightIcon />
-                            </button>
+                            </button>}
                         </Flex>
 
                     </motion.div>
