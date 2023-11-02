5f4d71bd8824db7dc85f7200b4abd679ba6df40c
eodrkd12 <eodrkd12@gmail.com>
2023-11-02 11:15:33 +0900

    Update brandPackageDesign-data.ts

--- src/app/@student/data/brandPackageDesign-data.ts
+++ src/app/@student/data/brandPackageDesign-data.ts
@@ -1,129 +1,130 @@
-export default [{
-    name: '키친 하테노',
+export default [
+  {
+    name: "키친 하테노",
     student: [
-        {
-            sname: '이규민',
-            englishName: 'Gyumin Lee',
-            studentNumber: '201720373',
-            email: 'envy9824@gmail.com'
-        }
+      {
+        sname: "이규민",
+        englishName: "Gyumin Lee",
+        studentNumber: "201720373",
+        email: "envy9824@gmail.com",
+      },
     ],
-    introduction: '열심과 결심은 밥심에서부터',
-    explanation: "사람은 시각, 촉각, 후각, 미각, 청각 다섯 가지 감각으로 세상을 체험한다. 하지만 보통의 창작물들은 시각적인 경험, 청각적인 경험에 국한돼 있다. 다섯 가지 감각이 모두 활용될 때 '실감'난다고 표현하므로 창작물이 가지는 이 한계를 극복해야 비로소 창작물을 온전히 즐겼다고 말할 수 있다. 따라서 약 1억 3천만 장의 판매량으로 입증된 게임 '젤다의 전설'의 세계관을, 오감을 자극하는 가장 보편화된 행동인 '요리'와 접목했을 때, 잘 만든 창작물을 잘 즐겼다는 만족이 촉발될 수 있다고 판단했다. 게임 속 음식을 현실에서도 맛볼 수 있도록 구현함으로써 창작물을 명징하게 음미할 기회를 제공하며, 단순한 게임 음식 브랜드가 아닌, 고된 하루하루를 버티는 현대인 '용사'의 소울 푸드 브랜드로 성장하고자 한다."
-    ,
+    introduction: "열심과 결심은 밥심에서부터",
+    explanation:
+      "사람은 시각, 촉각, 후각, 미각, 청각 다섯 가지 감각으로 세상을 체험한다. 하지만 보통의 창작물들은 시각적인 경험, 청각적인 경험에 국한돼 있다. 다섯 가지 감각이 모두 활용될 때 '실감'난다고 표현하므로 창작물이 가지는 이 한계를 극복해야 비로소 창작물을 온전히 즐겼다고 말할 수 있다. 따라서 약 1억 3천만 장의 판매량으로 입증된 게임 '젤다의 전설'의 세계관을, 오감을 자극하는 가장 보편화된 행동인 '요리'와 접목했을 때, 잘 만든 창작물을 잘 즐겼다는 만족이 촉발될 수 있다고 판단했다. 게임 속 음식을 현실에서도 맛볼 수 있도록 구현함으로써 창작물을 명징하게 음미할 기회를 제공하며, 단순한 게임 음식 브랜드가 아닌, 고된 하루하루를 버티는 현대인 '용사'의 소울 푸드 브랜드로 성장하고자 한다.",
     still: [
-        "/image/workimage/brandpackage/kitchen/still/1.jpg",
-        "/image/workimage/brandpackage/kitchen/still/2.jpg",
-        "/image/workimage/brandpackage/kitchen/still/3.jpg",
-        "/image/workimage/brandpackage/kitchen/still/4.jpg",
-        "/image/workimage/brandpackage/kitchen/still/5.jpg",
-        "/image/workimage/brandpackage/kitchen/still/6.jpg",
-        "/image/workimage/brandpackage/kitchen/still/7.jpg",
-        "/image/workimage/brandpackage/kitchen/still/8.jpg"
-    ]
-},
-{
-    name: '냠코 캠페인',
-    student: [
-        {
-            sname: '이태형',
-            englishName: 'Taehyung Lee',
-            studentNumber: '201720397',
-            email: 'gud2212@gmail.com'
-        }
+      "/image/workimage/brandpackage/kitchen/still/1.jpg",
+      "/image/workimage/brandpackage/kitchen/still/2.jpg",
+      "/image/workimage/brandpackage/kitchen/still/3.jpg",
+      "/image/workimage/brandpackage/kitchen/still/4.jpg",
+      "/image/workimage/brandpackage/kitchen/still/5.jpg",
+      "/image/workimage/brandpackage/kitchen/still/6.jpg",
+      "/image/workimage/brandpackage/kitchen/still/7.jpg",
+      "/image/workimage/brandpackage/kitchen/still/8.jpg",
     ],
-    introduction: '올바른 재활용의 첫 걸음',
-    explanation: '2021년 기준 291만 톤의 가정 플라스틱 폐기물이 버려지고 있지만, 재활용의 비율은 56.7%에 불과하다. 가정 플라스틱 폐기물 중 73%는 식품 포장재라는 사실을 바탕으로, 음식을 먹을 때 발생하는 플라스틱 쓰레기를 올바르게 재활용한다면 효과적인 해결 방법이 될 수 있을 것이라는 결론에 도달했다. 따라서 음식을 먹을 때 재활용 실천을 유도하는 "냠코 브랜드 캠페인"을 기획하였다. 냠코하자 캠페인은 냠코앱을 중심으로 진행된다. 앱에서 재활용 인증샷을 찍고 이벤트를 진행하며 재활용을 실천하면 포인트가 쌓인다. 이 포인트로 각종 상품, 냠코 굿즈들을 구매할 수 있다. 또한 여러 가지 미션을 달성하면 앱 내에서 뱃지를 받게 되고, 어떤 뱃지들은 실제로도 받을 수도 있다. 이외에도 배달 용기의 구체적인 재활용법이 적혀있는 재활용 카드, 밥 먹을 때 플라스틱을 덜 쓸 수 있게 하는 수저와 친환경 보관 용기 세트, 각종 팬시 용품을 사용하며 캠페인에 참여할 수 있다.'
-    ,
+  },
+  {
+    name: "냠코 캠페인",
+    student: [
+      {
+        sname: "이태형",
+        englishName: "Taehyung Lee",
+        studentNumber: "201720397",
+        email: "gud2212@gmail.com",
+      },
+    ],
+    introduction: "올바른 재활용의 첫 걸음",
+    explanation:
+      '2021년 기준 291만 톤의 가정 플라스틱 폐기물이 버려지고 있지만, 재활용의 비율은 56.7%에 불과하다. 가정 플라스틱 폐기물 중 73%는 식품 포장재라는 사실을 바탕으로, 음식을 먹을 때 발생하는 플라스틱 쓰레기를 올바르게 재활용한다면 효과적인 해결 방법이 될 수 있을 것이라는 결론에 도달했다. 따라서 음식을 먹을 때 재활용 실천을 유도하는 "냠코 브랜드 캠페인"을 기획하였다. 냠코하자 캠페인은 냠코앱을 중심으로 진행된다. 앱에서 재활용 인증샷을 찍고 이벤트를 진행하며 재활용을 실천하면 포인트가 쌓인다. 이 포인트로 각종 상품, 냠코 굿즈들을 구매할 수 있다. 또한 여러 가지 미션을 달성하면 앱 내에서 뱃지를 받게 되고, 어떤 뱃지들은 실제로도 받을 수도 있다. 이외에도 배달 용기의 구체적인 재활용법이 적혀있는 재활용 카드, 밥 먹을 때 플라스틱을 덜 쓸 수 있게 하는 수저와 친환경 보관 용기 세트, 각종 팬시 용품을 사용하며 캠페인에 참여할 수 있다.',
     still: [
-        "/image/workimage/brandpackage/namco/still/1.png",
-        "/image/workimage/brandpackage/namco/still/2.png",
-        "/image/workimage/brandpackage/namco/still/3.png",
-        "/image/workimage/brandpackage/namco/still/4.png",
-        "/image/workimage/brandpackage/namco/still/5.png",
-        "/image/workimage/brandpackage/namco/still/6.png",
-        "/image/workimage/brandpackage/namco/still/7.png",
-        "/image/workimage/brandpackage/namco/still/8.png"
-    ]
-},
-{
-    name: 'NUVID',
-    student: [
-        {
-            sname: '박소영',
-            englishName: 'Soyeong Park',
-            studentNumber: '202023433',
-            email: 'parksy12360@gmail.com'
-        }
+      "/image/workimage/brandpackage/namco/still/1.png",
+      "/image/workimage/brandpackage/namco/still/2.png",
+      "/image/workimage/brandpackage/namco/still/3.png",
+      "/image/workimage/brandpackage/namco/still/4.png",
+      "/image/workimage/brandpackage/namco/still/5.png",
+      "/image/workimage/brandpackage/namco/still/6.png",
+      "/image/workimage/brandpackage/namco/still/7.png",
+      "/image/workimage/brandpackage/namco/still/8.png",
     ],
-    introduction: '새로운 쾌활함을 보여주다',
-    explanation: "NUVID는 뉴트로를 컨셉으로 한 버츄얼 아이돌이다. NUVID는 뉴트로의 상큼함과 명랑함, 통통 튀는 귀여움을 키워드로 가지고 있다. 브랜드의 네이밍 또한 컨셉의 키워드에서 도출해 '새로운 쾌활함을 보여주다'라는 의미를 가지고 있다. 최근 엔터테인먼트 시장은 많은 아이돌과 더불어서 활발해진 영상매체로 다양한 버츄얼 아이돌의 활동이 눈에 띄게 많아지고 있어 엔터 시장은 팬들과의 원활한 소통을 기본으로 유행과 콘텐츠에 차별성을 중요시 생각한다. 때문에 일반적인 굿즈뿐만 아니라 증강현실을 통해 모션 패키지를 온 오프라인 모두에서 즐길 수 있게 하였다. 작은 차별로 NUVID의 브랜드 가치와 존재는 엔터시장에서 큰 성장이 될 것이다."
-    ,
+  },
+  {
+    name: "NUVID",
+    student: [
+      {
+        sname: "박소영",
+        englishName: "Soyeong Park",
+        studentNumber: "202023433",
+        email: "parksy12360@gmail.com",
+      },
+    ],
+    introduction: "새로운 쾌활함을 보여주다",
+    explanation:
+      "NUVID는 뉴트로를 컨셉으로 한 버츄얼 아이돌이다. NUVID는 뉴트로의 상큼함과 명랑함, 통통 튀는 귀여움을 키워드로 가지고 있다. 브랜드의 네이밍 또한 컨셉의 키워드에서 도출해 '새로운 쾌활함을 보여주다'라는 의미를 가지고 있다. 최근 엔터테인먼트 시장은 많은 아이돌과 더불어서 활발해진 영상매체로 다양한 버츄얼 아이돌의 활동이 눈에 띄게 많아지고 있어 엔터 시장은 팬들과의 원활한 소통을 기본으로 유행과 콘텐츠에 차별성을 중요시 생각한다. 때문에 일반적인 굿즈뿐만 아니라 증강현실을 통해 모션 패키지를 온 오프라인 모두에서 즐길 수 있게 하였다. 작은 차별로 NUVID의 브랜드 가치와 존재는 엔터시장에서 큰 성장이 될 것이다.",
     still: [
-        "/image/workimage/brandpackage/nuvid/still/1.jpg",
-        "/image/workimage/brandpackage/nuvid/still/2.jpg",
-        "/image/workimage/brandpackage/nuvid/still/3.jpg",
-        "/image/workimage/brandpackage/nuvid/still/4.jpg",
-        "/image/workimage/brandpackage/nuvid/still/5.jpg",
-        "/image/workimage/brandpackage/nuvid/still/6.jpg"
-    ]
-},
-{
-    name: '터울 Teoul',
-    student: [
-        {
-            sname: '김세현',
-            englishName: 'Saehyun Kim',
-            studentNumber: '201720263',
-            email: 'kimsaen111@gmail.com'
-        }
+      "/image/workimage/brandpackage/nuvid/still/1.jpg",
+      "/image/workimage/brandpackage/nuvid/still/2.jpg",
+      "/image/workimage/brandpackage/nuvid/still/3.jpg",
+      "/image/workimage/brandpackage/nuvid/still/4.jpg",
+      "/image/workimage/brandpackage/nuvid/still/5.jpg",
+      "/image/workimage/brandpackage/nuvid/still/6.jpg",
     ],
-    introduction: '천년의 터울을 넘어, 그 때의 조선으로',
-    explanation: '도시관광브랜드/로고, 앱, 그래픽, 패턴/웰컴키트(상자, 카드, 문구류, 향수병) 전통과 현대의 아름다움을 조화롭게 담고 있는 서울. 도심 속 자리 잡은 서울의 고궁은 마치 또 다른 세계로의 문을 열어주는 듯한 느낌을 선사한다. <터울>은 “천 년의 터울을 넘어 그때의 조선으로""라는 슬로건 아래, 서울의 궁궐 관광을 혁신적인 방식으로 접근하여 시대를 넘어 궁의 아름다움과 역사를 즐기는 경험을 선사하며 서울의 역사와 문화를 더욱 폭넓게 알리는 데에 힘쓴다. <터울>은 전통과 현대의 조화를 아름답게 담아내고 있다. 처마와 기와 등 궁궐의 건축적 모티브와 옛 한글의 형태, 오방색을 재해석하여 전통과 현대를 결합시킨 그래픽으로 브랜드의 독특한 정체성을 강조한다. 또한 NFC카드를 활용한 관광 어플과 AR기술을 접목한 안내 콘텐츠, 장소에 따른 추천 플레이리스트와 향 등을 통해 완벽한 몰입 경험을 제공한다.'
-},
-{
-    name: '젤라또 프리즈',
+  },
+  {
+    name: "터울 Teoul",
     student: [
-        {
-            sname: '최이영',
-            englishName: 'Iyeong Choi',
-            studentNumber: '201821742',
-            email: '20banana47@gmail.com'
-        }
+      {
+        sname: "김세현",
+        englishName: "Saehyun Kim",
+        studentNumber: "201720263",
+        email: "kimsaen111@gmail.com",
+      },
     ],
-    introduction: '독특함에 녹아들다',
-    explanation: '나를 당당하게 만드는 디저트. 젤라또 프리즈는 다양한 개성이 어우러지는 그래피티 감성의 젤라또 브랜드이다. 젤라또 프리즈는 디저트를 즐기는 공간을 넘어서 내가 나일 수 있는, 다양한 개성이 포용되는 편견으로부터 안전한 공간을 추구한다. 다양한 개성이 섞여 조화를 이루는 이곳에서 특별한 당신의 맛을 발견해보자 GELATO FREEZ의 네이밍은 젤라또의 감각을 불러일으키는 freeze, 자유롭고 당당한 사람들(프리-즈)라는 의미를 담았다 다양한 개성이 어우러진 믹스앤매치 조합의 로고로, 자체 제작한 타이포를 조합했다. 개성이 자유로울 수 있는 공간이란 브랜드 코어 밸류에 맞게 각기 다른 개성을 담은 26개의 알파벳 폰트를 제작하였다. 부드럽거나 딱딱하고, 여리여리하거나 강한 형태를 통해 알파벳들은 제각기 다른 매력을 드러낸다.'
-    ,
+    introduction: "천년의 터울을 넘어, 그 때의 조선으로",
+    explanation:
+      '도시관광브랜드/로고, 앱, 그래픽, 패턴/웰컴키트(상자, 카드, 문구류, 향수병) 전통과 현대의 아름다움을 조화롭게 담고 있는 서울. 도심 속 자리 잡은 서울의 고궁은 마치 또 다른 세계로의 문을 열어주는 듯한 느낌을 선사한다. <터울>은 “천 년의 터울을 넘어 그때의 조선으로""라는 슬로건 아래, 서울의 궁궐 관광을 혁신적인 방식으로 접근하여 시대를 넘어 궁의 아름다움과 역사를 즐기는 경험을 선사하며 서울의 역사와 문화를 더욱 폭넓게 알리는 데에 힘쓴다. <터울>은 전통과 현대의 조화를 아름답게 담아내고 있다. 처마와 기와 등 궁궐의 건축적 모티브와 옛 한글의 형태, 오방색을 재해석하여 전통과 현대를 결합시킨 그래픽으로 브랜드의 독특한 정체성을 강조한다. 또한 NFC카드를 활용한 관광 어플과 AR기술을 접목한 안내 콘텐츠, 장소에 따른 추천 플레이리스트와 향 등을 통해 완벽한 몰입 경험을 제공한다.',
+  },
+  {
+    name: "젤라또 프리즈",
+    student: [
+      {
+        sname: "최이영",
+        englishName: "Iyeong Choi",
+        studentNumber: "201821742",
+        email: "20banana47@gmail.com",
+      },
+    ],
+    introduction: "독특함에 녹아들다",
+    explanation:
+      "나를 당당하게 만드는 디저트. 젤라또 프리즈는 다양한 개성이 어우러지는 그래피티 감성의 젤라또 브랜드이다. 젤라또 프리즈는 디저트를 즐기는 공간을 넘어서 내가 나일 수 있는, 다양한 개성이 포용되는 편견으로부터 안전한 공간을 추구한다. 다양한 개성이 섞여 조화를 이루는 이곳에서 특별한 당신의 맛을 발견해보자 GELATO FREEZ의 네이밍은 젤라또의 감각을 불러일으키는 freeze, 자유롭고 당당한 사람들(프리-즈)라는 의미를 담았다 다양한 개성이 어우러진 믹스앤매치 조합의 로고로, 자체 제작한 타이포를 조합했다. 개성이 자유로울 수 있는 공간이란 브랜드 코어 밸류에 맞게 각기 다른 개성을 담은 26개의 알파벳 폰트를 제작하였다. 부드럽거나 딱딱하고, 여리여리하거나 강한 형태를 통해 알파벳들은 제각기 다른 매력을 드러낸다.",
     still: [
-        "/image/workimage/brandpackage/gelato/still/1.jpg",
-        "/image/workimage/brandpackage/gelato/still/2.jpg",
-        "/image/workimage/brandpackage/gelato/still/3.jpg",
-        "/image/workimage/brandpackage/gelato/still/4.jpg",
-        "/image/workimage/brandpackage/gelato/still/5.jpg",
-        "/image/workimage/brandpackage/gelato/still/6.jpg",
-        "/image/workimage/brandpackage/gelato/still/7.jpg"
-    ]
-},
-{
-    name: 'PATME',
-    student: [
-        {
-            sname: '김예서',
-            englishName: 'Yehsuh Kim',
-            studentNumber: '202020020',
-            email: 'yehsuh7762@gmail.com'
-        }
+      "/image/workimage/brandpackage/gelato/still/1.JPG",
+      "/image/workimage/brandpackage/gelato/still/2.JPG",
+      "/image/workimage/brandpackage/gelato/still/3.JPG",
+      "/image/workimage/brandpackage/gelato/still/4.JPG",
+      "/image/workimage/brandpackage/gelato/still/5.JPG",
+      "/image/workimage/brandpackage/gelato/still/6.JPG",
+      "/image/workimage/brandpackage/gelato/still/7.JPG",
     ],
-    introduction: '나의 반려동물과 함께',
-    explanation: '반려동물 1500만 시대. 반려동물은 단순히 기르는 개념이 아닌 인생을 함께 살아갈 가족으로 자리 잡힌 지 오래이다. <PATME 파트메>는 사람들이 반려동물에 대한 이해 없이 무책임하게 그들을 데려오는 것을 막고자 예비 반려인들이 입양 전 그들에게 자격이 있는지 검증해 주며 사전 지식을 알려주는 교육 브랜드이다. <PATME>는 PAT과 ME를 결합한 합성어로 나를 쓰다듬어줘, 아껴줘라는 뜻을 가지고 있으며, PAT ‘쓰다듬다’와 PET ‘반려동물’의 영어 발음의 유사성을 이용하여 브랜드의 아이덴티티를 명료하게 표현했다. 반려동물과 반려인은 주종 관계가 아닌 ‘파트너’라는 느낌을 살려 디자인한 로고이다. <PATME 파트메>의 교육 과정을 모두 수료하면 수료증을 증거로 복잡한 절차 없이 반려동물을 입양하고 그들의 정보를 쉽게 접할 수 있다.'
-    ,
+  },
+  {
+    name: "PATME",
+    student: [
+      {
+        sname: "김예서",
+        englishName: "Yehsuh Kim",
+        studentNumber: "202020020",
+        email: "yehsuh7762@gmail.com",
+      },
+    ],
+    introduction: "나의 반려동물과 함께",
+    explanation:
+      "반려동물 1500만 시대. 반려동물은 단순히 기르는 개념이 아닌 인생을 함께 살아갈 가족으로 자리 잡힌 지 오래이다. <PATME 파트메>는 사람들이 반려동물에 대한 이해 없이 무책임하게 그들을 데려오는 것을 막고자 예비 반려인들이 입양 전 그들에게 자격이 있는지 검증해 주며 사전 지식을 알려주는 교육 브랜드이다. <PATME>는 PAT과 ME를 결합한 합성어로 나를 쓰다듬어줘, 아껴줘라는 뜻을 가지고 있으며, PAT ‘쓰다듬다’와 PET ‘반려동물’의 영어 발음의 유사성을 이용하여 브랜드의 아이덴티티를 명료하게 표현했다. 반려동물과 반려인은 주종 관계가 아닌 ‘파트너’라는 느낌을 살려 디자인한 로고이다. <PATME 파트메>의 교육 과정을 모두 수료하면 수료증을 증거로 복잡한 절차 없이 반려동물을 입양하고 그들의 정보를 쉽게 접할 수 있다.",
     still: [
-        "/image/workimage/brandpackage/patme/still/1.jpg",
-        "/image/workimage/brandpackage/patme/still/2.jpg",
-        "/image/workimage/brandpackage/patme/still/3.jpg",
-        "/image/workimage/brandpackage/patme/still/4.jpg",
-        "/image/workimage/brandpackage/patme/still/5.jpg",
-    ]
-}
-
-]
+      "/image/workimage/brandpackage/patme/still/1.jpg",
+      "/image/workimage/brandpackage/patme/still/2.jpg",
+      "/image/workimage/brandpackage/patme/still/3.jpg",
+      "/image/workimage/brandpackage/patme/still/4.jpg",
+      "/image/workimage/brandpackage/patme/still/5.jpg",
+    ],
+  },
+];
